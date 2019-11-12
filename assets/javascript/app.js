3783bb226e414d25f9f0dc97ad3ace22ff2b47b6
Max Power <dbroncosfan@gmail.com>
2019-11-11 22:54:18 -0700

    index.html and app.js adds

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+var triviaQuestions = [{
+    question: "In Egyptian hieroglyphs, the symbol of a decorated eye most commonly represents the Eye of which god?",
+    answers: ["Horus", "Amun-Ra", "Osiris", "Anubis"]
+},{
+    question: "Edison's Electric Pen became the inspiration for which modern-day tool?",
+    answers: ["Electric Tattoo Gun", "Record Player", "Electric Sewing Machine", "Electric Leather Punch"]
+},{
+    question: "Which South American desert is one of the driest places on Earth, never having recorded a single drop of rain?",
+    answers: ["Atacama Desert", "Death Valley Desert", "Kalahari Desert", "Great Basin Desert"]
+},{
+    question: "Which med student's travels, before he became a Marxist icon, are detailed in the book 'The Motorcycle Diaries'?",
+    answers: ["Ernesto Che Guevara", "Fidel Castro", "John Dillinger", "Hugo Chavez"]
+},{
+    question: "Who is John Galt?",
+    answers: ["A fictional character in 'Atlas Shrugged'", "The 6th president of the United States", "Jack the Rippers real name", "A pseudonym for Ayn Rand"]
+},{
+    question: "In the book '1984', what does Winston trace in the dust on the table at the end of the novel?",
+    answers: ["2+2=5", "I love Big Brother", "Death to Big Brother", "Room 101"]
+}];
+
