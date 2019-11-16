f5c32cfc951566996ba26790db503cee504dff90
Max Power <dbroncosfan@gmail.com>
2019-11-15 23:17:37 -0700

    semi working game, questions and answer array load on screen and timer starts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,106 @@
-var triviaQuestions = [{
-    question: "In Egyptian hieroglyphs, the symbol of a decorated eye most commonly represents the Eye of which god?",
-    answers: ["Horus", "Amun-Ra", "Osiris", "Anubis"]
+$(document).ready(function () {
+
+var questions = [{
+    question: "In the book Animal Farm, who teaches the sheep to chant 'four legs good, 2 legs bad?'",
+    answer: ["Napoleon", "Boxer", "Squealer", "Clover"],
+    name: "legs",
+    correct: "Squealer",
+    class: ".legs"
 },{
-    question: "Edison's Electric Pen became the inspiration for which modern-day tool?",
-    answers: ["Electric Tattoo Gun", "Record Player", "Electric Sewing Machine", "Electric Leather Punch"]
+    question: "In the book 'The Prince,' what is Machiavelli's approach to the use of cruelty?",
+    answer: ["Never inflict cruelty at all", "Inflict small amounts of cruelty consistently", "Inflict all the cruelty at once, then never again", "Inflict cruelty suddenly and unpredictably"],
+    name: "prince",
+    correct: "Inflict all the cruelty at once, then never again",
+    class: ".prince"
 },{
-    question: "Which South American desert is one of the driest places on Earth, never having recorded a single drop of rain?",
-    answers: ["Atacama Desert", "Death Valley Desert", "Kalahari Desert", "Great Basin Desert"]
+    question: "In the book 'The Odyessy,' Menelaus is king of which city?",
+    answer: ["Athens", "Argos", "Pylos", "Sparta"],
+    name: "odyessy",
+    correct: "Atacama Desert",
+    class: ".odyessy"
 },{
     question: "Which med student's travels, before he became a Marxist icon, are detailed in the book 'The Motorcycle Diaries'?",
-    answers: ["Ernesto Che Guevara", "Fidel Castro", "John Dillinger", "Hugo Chavez"]
+    answer: ["Ernesto Che Guevara", "Fidel Castro", "John Dillinger", "Hugo Chavez"],
+    name: "student",
+    correct: "Ernesto Che Guevara",
+    class: ".student"
 },{
     question: "Who is John Galt?",
-    answers: ["A fictional character in 'Atlas Shrugged'", "The 6th president of the United States", "Jack the Rippers real name", "A pseudonym for Ayn Rand"]
+    answer: ["The 6th president of the United States", "Jack the Rippers real name", "A pseudonym for Ayn Rand", "A fictional character in 'Atlas Shrugged'"],
+    name: "galt",
+    correct: "A fictional character in 'Atlas Shrugged'",
+    class: ".galt"
 },{
     question: "In the book '1984', what does Winston trace in the dust on the table at the end of the novel?",
-    answers: ["2+2=5", "I love Big Brother", "Death to Big Brother", "Room 101"]
-}];
+    answer: ["I love Big Brother", "2+2=5", "Death to Big Brother", "Room 101"],
+    name: "winston",
+    correct: "2+2=5",
+    class: ".winston"
+},
+]
 
+var choices = ["first", "second", "third", "fourth"];
+
+$(".startGame").on("click", function() {
+    countdown(40);
+    showQuestions();
+});
+
+var showQuestions = function () {
+    $(".questions").empty();
+    for (var i = 0; i < 6; i++) {
+        $('.questions').prepend('<div class="' + questions[i].name + '"></div>');
+        $(questions[i].class).append('<div class ="ques-title">' + questions[i].question + '</div>');
+
+        for (var j = 0; j <= 3; j++) {
+            $(questions[i].class).append("<input type='radio' name='" + questions[i].name + "' value='" + questions[i].answer[j] + "'/><label for='" + choices[j] + "'>" + questions[i].answer[j] + "</label>");
+        }
+        $(".questions").prepend("<hr />");
+        }
+    }
+
+var countdown = function(seconds) {
+    var timer = setInterval(function () {
+        seconds = seconds - 1;
+        $("#timer").html(seconds);
+
+        if (seconds <= 0) {
+            $(".container").fadeOut(500);
+            var correctAnswers = 0;
+            var wrongAnswers = 0;
+
+            for(var i = 0; i < 6; i++) {
+                if($("input:radio[name='" + questions[i].name + "']:checked").val() === questions[i].correct) {
+                    correctAnswers++;
+                } else {
+                    wrongAnswers++;
+        };
+    }
+    clearInterval(timer);
+    return;
+}
+},1000);
+
+$("#submit").on("click", function() {   
+    clearInterval(timer);
+})
+
+};
+
+var results = $("#submit").on("click", function() {
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+
+    for (var i = 0; i < 6; i++) {
+        if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+            correctAnswers++;
+        } else {
+            wrongAnswers++;
+        };
+    };
+    countdown();
+
+})
+
+
+})
